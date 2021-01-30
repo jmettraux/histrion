d22702aaa74909445dc2f1480a0ba8113472d1e9
John Mettraux <jmettraux@gmail.com>
2021-01-30 19:36:01 +0900

    Apply background

--- lib/creagen/creature.rb
+++ lib/creagen/creature.rb
@@ -1,8 +1,20 @@
 
 class Creagen::Creature
 
+  SKILLS = %w[
+    Administer Connect Convince Craft Exert Heal Hunt Know Lead Notice Perform
+    Pray Ride Sail Sneak Survive Trade Work ]
+  PHYSICAL_ATTRIBUTES = %w[
+    strength constitution dexterity ]
+  MENTAL_ATTRIBUTES = %w[
+    intelligence wisdom charisma ]
+
   attr_accessor :strength, :constitution, :dexterity
   attr_accessor :intelligence, :wisdom, :charisma
+
+  attr_accessor :klass, :level
+
+  attr_reader :skills
 
   alias str= strength=
   alias con= constitution=
@@ -41,11 +53,30 @@ class Creagen::Creature
     self.int = dice.roll
     self.wis = dice.roll
     self.cha = dice.roll
+
+    @level = 1
+
+    @skills = {}
   end
 
-  def background=(b)
+  def stab; @skills['Stab'] || -2; end
+  def shoot; @skills['Shoot'] || -2; end
+  def punch; @skills['Punch'] || -2; end
+
+  def background=(b, meth=nil)
 
     @background = b[:name]
+
+    inc_skill(b[:free])
+
+    meth =
+      meth ?
+      "apply_background_#{meth}" :
+      [ :apply_background_quick,
+        :apply_background_learn,
+        :apply_background_roll ].shuffle(random: @rnd).first
+
+    self.send(meth, b)
   end
 
   protected
@@ -62,5 +93,93 @@ class Creagen::Creature
     else 3
     end
   end
+
+  def apply_background_quick(b)
+
+    b[:quick].each { |s| inc_skill(s) }
+  end
+
+  def apply_background_learn(b)
+
+    skills = []
+    while skills.count < 2
+      s = b[:learning].shuffle(random: @rnd).first
+      skills << s unless s.start_with?('Any ')
+    end
+
+    skills.each { |s| inc_skill(s) }
+  end
+
+  def apply_background_roll(b)
+
+    3.times do
+      self.send(
+        [ :apply_background_roll_growth, :apply_background_roll_learning ]
+          .shuffle(random: @rnd)
+          .first,
+        b)
+    end
+  end
+
+  def apply_background_roll_growth(b)
+
+    case g = pick(b[:growth])
+    when /^\+/ then inc_attribute(g)
+    when /^Any Combat$/i then grow_combat_skill
+    when /^Any Skill$/i then grow_any_skill
+    else inc_skill(g)
+    end
+  end
+
+  def inc_attribute(s)
+
+#p [ :inc_attribute, s ]
+    m = s.match(/^\+(\d+) (.+)$/)
+#p [ :inc_attribute, m[1], m[2] ]
+
+    atts =
+      case m[2]
+      when /Physical/ then PHYSICAL_ATTRIBUTES
+      when /Mental/ then MENTAL_ATTRIBUTES
+      else PHYSICAL_ATTRIBUTES + MENTAL_ATTRIBUTES
+      end
+
+    m[1].to_i.times do
+      att = pick(atts)
+      instance_eval "@#{att} = @#{att} + 1"
+    end
+  end
+
+  def grow_combat_skill
+
+    inc_skill(pick(%w[ Stab Shoot Punch ]))
+  end
+
+  def grow_any_skill
+
+    inc_skill(pick(SKILLS))
+  end
+
+  def apply_background_roll_learning(b)
+
+    case l = pick(b[:learning])
+    when /^Any Combat$/i then grow_combat_skill
+    when /^Any Skill$/i then grow_any_skill
+    else inc_skill(l)
+    end
+  end
+
+  def inc_skill(s)
+
+    l = @skills[s] || -1
+
+    return false if l > 0
+
+    @skills[s] = l + 1
+
+    true
+  end
+
+  def pick(a); a.shuffle(random: @rnd).first; end
 end
 
