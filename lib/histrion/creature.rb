45d86e7119f0419d5406563313b3dbd1f1810e7c
John Mettraux <jmettraux@gmail.com>
2021-02-04 20:27:22 +0900

    Fix shooting attack bonuses

--- lib/histrion/creature.rb
+++ lib/histrion/creature.rb
@@ -246,7 +246,7 @@ class Histrion::Creature
             r0, r1 = r.collect { |e| FeetExpander.exp(e) }
             r1 = ' ' + r1 if r1.match(/^\d\df/)
             nic = w[:nick][0, nwidth]
-            att0 = stab + dex_mod + ab
+            att0 = shoot + dex_mod + ab
             att1 = att0 - 2
             dmg = w[:damage] + dex_mod_s
             v = "%#{nwidth + 2}s  DEX  %s ->%-18s %s ->%-19s  dmg %s" % [
